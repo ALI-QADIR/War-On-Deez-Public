e117603c98330cb8e9fd658f80de7d12b583f03b
Ali-Qadir <aliqadir786.is@gmail.com>
2023-04-19 23:42:50 +0530

    Add Resolution Settings

--- UI/OptionsMenu.cs
+++ UI/OptionsMenu.cs
@@ -1,9 +1,37 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private TMP_Dropdown _resolutionDropdown;
+    [SerializeField] private TMP_Dropdown _qualityDropdown;
+
+    private Resolution[] _resolutions;
+
+    private void Awake()
+    {
+        _resolutions = Screen.resolutions;
+        _resolutionDropdown.ClearOptions();
+        var options = new List<string>();
+        var currentResolutionIndex = 0;
+        for (var i = 0; i < _resolutions.Length; i++)
+        {
+            var option = $"{_resolutions[i].width} x {_resolutions[i].height}";
+            options.Add(option);
+            if (_resolutions[i].width == Screen.currentResolution.width &&
+                _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        _resolutionDropdown.AddOptions(options);
+        _resolutionDropdown.value = currentResolutionIndex;
+        _resolutionDropdown.RefreshShownValue();
+    }
 
     public void SetMaster(float volume)
     {
@@ -28,5 +56,11 @@ public class OptionsMenu : MonoBehaviour
     public void SetFullscreen(bool fullscreen)
     {
         Screen.fullScreen = fullscreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        var resolution = _resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
