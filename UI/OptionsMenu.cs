863d80f77d4b3e224068ba7292fa851e0ba5dd24
Ali-Qadir <aliqadir786.is@gmail.com>
2023-04-19 22:12:49 +0530

    Add Graphics settings, fullscreen toggle

--- UI/OptionsMenu.cs
+++ UI/OptionsMenu.cs
@@ -3,20 +3,30 @@ using UnityEngine.Audio;
 
 public class OptionsMenu : MonoBehaviour
 {
-    [SerializeField] private AudioMixer audioMixer;
+    [SerializeField] private AudioMixer _audioMixer;
 
     public void SetMaster(float volume)
     {
-        audioMixer.SetFloat("Master", volume);
+        _audioMixer.SetFloat("Master", volume);
     }
 
     public void SetMusic(float volume)
     {
-        audioMixer.SetFloat("Music", volume);
+        _audioMixer.SetFloat("Music", volume);
     }
 
     public void SetSFX(float volume)
     {
-        audioMixer.SetFloat("SFX", volume);
+        _audioMixer.SetFloat("SFX", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen(bool fullscreen)
+    {
+        Screen.fullScreen = fullscreen;
     }
 }
